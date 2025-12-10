efd5bd667e57e36230be261366622ed3357679cf
Anita Koli <kolianita784@gmail.com>
2025-12-10 19:15:34 +0530

    Add Celsius to Fahrenheit conversion function
    Implement function to convert Celsius to Fahrenheit.

---
+++ celsius_Fahrenheit.js
@@ -0,0 +1,13 @@
+function convertCtoF(cels){
+  return cels*(9/5)+32;
+}
+console.log(convertCtoF(0));
+console.log(convertCtoF(-30));
+console.log(convertCtoF(-10));
+console.log(convertCtoF(20));
+console.log(convertCtoF(30));
+/*32
+-22
+14
+68
+86*/
