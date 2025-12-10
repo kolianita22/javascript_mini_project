014cf9a3d385e2705f92f7752ef7cc7f5340294d
Anita Koli <kolianita784@gmail.com>
2025-12-10 11:37:33 +0530

    Add TriviaBot with coding fun facts

---
+++ TriviaBot.js
@@ -0,0 +1,21 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName="TriviaBot";
+const botLocation="the internet";
+const favoriteLanguage="JavaScript";
+
+
+console.log(`My name is ${botName} and I live on ${botLocation}.`);
+console.log(`My favorite programming language is ${favoriteLanguage}.`);
+
+let codingFact=`My favorite language is ${favoriteLanguage} because it allows me to create interactive web experiences.`;
+console.log(codingFact);
+
+codingFact=`Did you know that ${favoriteLanguage} was created in just 10 days by Brendan Eich in 1995?`;
+console.log(codingFact);
+
+codingFact=`Another fun fact: ${favoriteLanguage} is the most commonly used language for web development, powering over 95% of all websites!`;
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you.Goodbye!"+botName+"from"+botLocation+".");
+
