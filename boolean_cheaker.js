d5dd585d99621c30378626e1895b79a204845cdc
Anita Koli <kolianita784@gmail.com>
2025-12-10 18:21:08 +0530

    Update console.log statements for boolean checks

--- boolean_cheaker.js
+++ boolean_cheaker.js
@@ -7,15 +7,15 @@ function booWho(value)
     return false;
   }
 }
-console.log(booWho(true));
-console.log(booWho(false));
-console.log(booWho([1,2,3]));
-console.log(booWho({"a":1}));
-console.log(booWho(1));
-console.log(booWho(NaN));
-console.log(booWho("a"));
-console.log(booWho("true"));
-console.log(booWho("false"));
+console.log(booWho(true));//true
+console.log(booWho(false));//true
+console.log(booWho([1,2,3]));//false
+console.log(booWho({"a":1}));//false
+console.log(booWho(1));//false
+console.log(booWho(NaN));//false
+console.log(booWho("a"));//false
+console.log(booWho("true"));//false
+console.log(booWho("false"));//false
 
 
 
