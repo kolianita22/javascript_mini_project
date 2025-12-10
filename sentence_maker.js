fc1cf6630e383e84a2464cdb8d489b97cc86534c
Anita Koli <kolianita784@gmail.com>
2025-12-10 12:10:46 +0530

    Create sentence_maker.js

---
+++ sentence_maker.js
@@ -0,0 +1,26 @@
+
+
+let adjective="cute";
+let noun="cat";
+let verb="running around";
+
+
+let place="Home";
+let adjective2="tiny";
+let noun2="fish";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun +" who loved to eat " + noun2 + ". The " + noun + " lived in a " + place +" and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: "+firstStory);
+
+
+adjective="small";
+noun="dog";
+verb="playing";
+place="garden";
+adjective2="fluffy";
+noun2="cookies";
+
+
+let secondStory="Once upon a time, there was a(n) " + adjective + " " + noun +" who loved to eat " + noun2 + ". The " + noun + " lived in a " + place +" and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+ console.log("Second story: "+secondStory);
