7e07031048820bbc3fcff11b385357ad20eba352
Anita Koli <kolianita784@gmail.com>
2025-12-22 18:17:01 +0530

    Create DNA_pair.js

---
+++ DNA_pair.js
@@ -0,0 +1,16 @@
+function pairElement(str) {
+  const pairs = {
+    A: "T",
+    T: "A",
+    C: "G",
+    G: "C"
+  };
+
+  const result = [];
+
+  for (let char of str) {
+    result.push([char, pairs[char]]);
+  }
+
+  return result;
+}
